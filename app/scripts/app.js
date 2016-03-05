f530f9a3acf5680616c56276c1c5617d9c71c286
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-05 23:13:08 +0100

    Init repository with Yeoman generated AngularJS project

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bookingSystemApp
+ * @description
+ * # bookingSystemApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bookingSystemApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
